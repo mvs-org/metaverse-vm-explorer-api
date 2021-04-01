a6a4962394108161d4d223b8ed97856bacbde093
canguruhh <sven@mutzl.org>
2021-04-01 10:19:10 +0200

    get latest balance from rpc

---
+++ src/config/web3.ts
@@ -0,0 +1,1 @@
+export const WEB3_URL = (process.env.WEB3_URL) ? process.env.WEB3_URL : 'https://vm.mvs.org/mainnet_rpc/'
--- src/controllers/address.controller.ts
+++ src/controllers/address.controller.ts
@@ -1,25 +1,33 @@
 import { Request, Response } from 'express'
 import { ResponseError, ResponseSuccess } from './../helpers/message.helper'
 import { TransactionModel } from '../models/transaction.model'
+import Web3 from 'web3'
+import { WEB3_URL } from '../config/web3'
 
 export class AddressController {
+
+  static web3 = new Web3(WEB3_URL)
 
   public async getAddress(req: Request, res: Response) {
     const address: string = req.params.address
     try {
 
-      const outcount = await TransactionModel.countDocuments({
-        from: address,
-      })
-      const incount = await TransactionModel
-      .countDocuments({
-        to: address,
-      })
-      res.setHeader('Cache-Control', 'public, max-age=600, s-maxage=600')
+      const [outcount, incount, balance] = await Promise.all([
+        TransactionModel.countDocuments({
+          from: address,
+        }),
+        TransactionModel
+          .countDocuments({
+            to: address,
+          }),
+          AddressController.web3.eth.getBalance(address),
+      ])
+      res.setHeader('Cache-Control', 'public, max-age=20, s-maxage=20')
       return res.json(new ResponseSuccess({
         sent: outcount,
         received: incount,
         address,
+        balance,
       }))
     } catch (err) {
       console.error(err)
